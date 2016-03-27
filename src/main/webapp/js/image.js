89e4977525f62dffb995890293237c88371f97b9
Jared Harper <jared.harper@gmail.com>
2016-03-27 11:14:52 -0500

    Fix bug in image loader

--- src/main/webapp/js/image.js
+++ src/main/webapp/js/image.js
@@ -1,10 +1,18 @@
+/*
+ * https://github.com/jamescharlesworth/jQuery-Image-Loader-Callback
+ * 
+ * With some changes (added attempts counter and increased callback timeout to 500ms, otherwise
+ * for missing images it will attempt an infinite number of almost unthrottled connections)
+ */
 (function( $ ) {
-  $.fn.loadImage = function(src,cb,image) {
+  $.fn.loadImage = function(src,cb,image,attempts) {
+
     var self = this,
         dataSrc = $(self).attr("data-src");
       
     image = image || new Image();
     cb = cb || function() {};
+    attempts = attempts || 1;
 
     if (typeof src === "undefined") {
       if (dataSrc.length) {
@@ -16,10 +24,12 @@
     setTimeout(function() {
       if (image.src != src)
         image.src = src;
+      if (attempts > 5)
+    	  return;
       if (!image.complete)
-        return self.loadImage(src,cb,image);
+        return self.loadImage(src,cb,image,attempts+1);
       self.attr('src',src);
       cb.call(self);
-    },50);
+    },500);
   };
 })( jQuery );
