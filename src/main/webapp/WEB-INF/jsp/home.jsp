2dd801f25a98d8650a431acb10b00969927ad914
Jared Harper <jared.harper@gmail.com>
2016-03-27 10:29:12 -0500

    Image "not finished loading" spinner thing added

--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -14,6 +14,7 @@
 <script src="js/jquery-2.2.0.min.js"></script>
 <script src="js/Chart.min.js"></script>
 <script src="js/spin.min.js"></script>
+<script src="js/image.js"></script>
 <script src="js/main.js"></script>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -148,8 +149,6 @@ $(document).ready(function() {
 	<!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -121,48 +121,6 @@ function showOpponentInfo() {
 }
 
 /**
- * Instantiate the loading gif with options and return
- * the spinner so it can be stopped by the caller.
- * 
- * Spin.js from: https://fgnass.github.io/spin.js/
- * 
- * And yes, it is a Stargate reference.  Nerd.
- * 
- */
-function makeItSpin(targetId) {
-
-	// Direct from spin.js docs because I don't do frontend well
-	var opts = {
-			  lines: 17				// The number of lines to draw
-			, length: 56			// The length of each line
-			, width: 14				// The line thickness
-			, radius: 42			// The radius of the inner circle
-			, scale: 3.5			// Scales overall size of the spinner
-			, corners: 1			// Corner roundness (0..1)
-			, color: '#6E6E6E'		// #rgb or #rrggbb or array of colors
-			, opacity: 0.4			// Opacity of the lines
-			, rotate: 0				// The rotation offset
-			, direction: 1			// 1: clockwise, -1: counterclockwise
-			, speed: 1.5			// Rounds per second
-			, trail: 60				// Afterglow percentage
-			, fps: 20				// Frames per second when using setTimeout() as a fallback for CSS
-			, zIndex: 2e9			// The z-index (defaults to 2000000000)
-			, className: 'spinner'	// The CSS class to assign to the spinner
-			, top: '50%'			// Top position relative to parent
-			, left: '50%'			// Left position relative to parent
-			, shadow: false			// Whether to render a shadow
-			, hwaccel: false		// Whether to use hardware acceleration
-			, position: 'absolute'	// Element positioning
-	}
-	
-	var target = document.getElementById(targetId)
-	var spinner = new Spinner(opts).spin(target);
-
-	return spinner;
-	
-}
-
-/**
  * Based on the selected criteria, get the chosen representative and display
  * their information, then get their sim/dims from the similarity matrix and
  * display those as well
@@ -173,9 +131,6 @@ function makeItSpin(targetId) {
  *            optional id to prepopulate home page
  */
 function showNewRepInfo(optionalId, page) {
-	
-	// Start loading gif
-	var spinner = makeItSpin('mainContent');
 	
 	// Clean out the old rep
 	$("#detail").find('div').remove().end();
@@ -260,11 +215,16 @@ function showNewRepInfo(optionalId, page) {
 				    id: v[i].id,
 				    'class' : 'col-md-4 col-sm-6 col-xs-12'
 				}));				
-				$(jqId).append($('<img>', {
+				var ni = $('<img>', {
 					id: "img" + v[i].id,
-					src: "img/" + v[i].id + ".jpg",
+					src: "img/loader.gif",
 					style: "cursor: pointer"
-				}));			
+				});
+				
+				// Loading indicator for image
+				$(ni).attr("data-src","img/" + v[i].id + ".jpg");
+				$(ni).loadImage();
+				$(jqId).append(ni);
 				
 				// Pic is a link to the voter's details page
 				$("#img" + v[i].id).click(function(val) {
@@ -291,6 +251,4 @@ function showNewRepInfo(optionalId, page) {
 		});
 	});	
 	
-	// Stop loading gif
-	spinner.stop();
 }
